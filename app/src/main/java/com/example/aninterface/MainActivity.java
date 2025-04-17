cedf9bd056e0fff24b6c33a0cb34aa3edeaf16a0
45_ladno_46 <dilanesteroff@gmail.com>
2025-04-17 20:52:53 +0300

    оттак.2

--- app/src/main/java/com/example/aninterface/MainActivity.java
+++ app/src/main/java/com/example/aninterface/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.aninterface;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +12,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private final Button buttonOffline = findViewById(R.id.offlineButton);
+    private final Button buttonOnline = findViewById(R.id.onlineButton);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,22 +26,13 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        Button buttonOffline = findViewById(R.id.offlineButton);
-        Button buttonOnline = findViewById(R.id.onlineButton);
-
-        buttonOffline.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intention = new Intent(MainActivity.this, OfflineActivity.class);
-                startActivity(intention);
-            }
+        buttonOffline.setOnClickListener(view -> {
+            Intent intention = new Intent(MainActivity.this, OfflineActivity.class);
+            startActivity(intention);
         });
-        buttonOnline.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intention = new Intent(MainActivity.this, OnlineActivity.class);
-                startActivity(intention);
-            }
+        buttonOnline.setOnClickListener(view -> {
+            Intent intention = new Intent(MainActivity.this, OnlineActivity.class);
+            startActivity(intention);
         });
     }
 
